2b371ad12594ac64fceb2b7db9b4e38a87e1717b
Anneli Hrginen <anneli.harginen@tktk.ee>
2023-05-20 11:00:24 +0300

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userCoice = Console.ReadLine();
+
+if (userCoice == "in")
+{
+
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, word!");
+}
+  
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
